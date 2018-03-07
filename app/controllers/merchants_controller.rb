1d2840e74796b5bc2d1d47018703cf8b558e0a60
Simon nguyen <simonnguyen@MacBook-Pro-3.local>
2018-03-06 21:27:01 -0800

    extract redundancy and prepare for production

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,15 @@
 class MerchantsController < ApplicationController
+  before_action :set_merchant, only: [:edit, :update, :show, :destroy]
+
+  def index 
+    @merchants = Merchant.all
+  end 
+
   def new
     @merchant = Merchant.new
+  end
+
+  def edit
   end
 
   def create 
@@ -14,12 +23,30 @@ class MerchantsController < ApplicationController
     end
   end
 
+  def update
+    if @merchant.update(merchant_params)
+      flash[:notice] = "Merchant was successfully updated"
+      redirect_to merchant_path(@merchant)
+    else 
+      render 'edit'
+    end
+  end 
+
   def show
-    @merchant = Merchant.find(params[:id])
+  end 
+
+  def destroy
+    @merchant.destroy
+    flash[:notice] = "Merchant was successfully deleted"
+    redirect_to merchants_path
   end 
 
   #Need to whitelist merchant params
   private
+    def set_merchant 
+      @merchant = Merchant.find(params[:id])
+    end
+
     def merchant_params
       params.require(:merchant).permit(:name, :description)
     end
